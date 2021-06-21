a5144e6f81b060c6d83caa84a53c6a27683f945b
Saurabh Dixit <43204978+sdixit07@users.noreply.github.com>
2021-06-21 16:51:30 +0530

    removed extent reporting

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -3,10 +3,11 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+@CucumberOptions(plugin = {"pretty", "html:Report/Cucumber.html"},
         glue = "stepdefinitions",
         features = "./src/main/resources/Features/",
         tags = "UI")
 
 public class TestRunner {
 }
+    
