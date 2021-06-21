538524901da3ceadc2dc16f5417461ccb4139b56
Saurabh Dixit <43204978+sdixit07@users.noreply.github.com>
2021-06-21 17:24:07 +0530

    updates

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -6,8 +6,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(plugin = {"pretty", "html:Report/Cucumber.html"},
         glue = "stepdefinitions",
         features = "./src/main/resources/Features/",
-        tags = "UI")
+        tags = "@API,@UI")
 
 public class TestRunner {
 }
-    
